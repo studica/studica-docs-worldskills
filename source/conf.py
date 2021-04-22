1c60ba10989116dd1093b5e7f9d4489643d05dc7
James.Taylor <James.Taylor@studica.com>
2021-04-22 10:37:01 -0400

    Turn CSS back on

--- source/conf.py
+++ source/conf.py
@@ -86,6 +86,6 @@ StandaloneHTMLBuilder.supported_image_types = [
     'image/jpg'
 ]
 
-#def setup(app):
-    #app.add_css_file('css/studica-rtd.css')
+def setup(app):
+    app.add_css_file('css/studica-rtd.css')
 
